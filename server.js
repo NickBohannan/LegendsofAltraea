6734b9f0a044db591927ca9b73c7ff5b0b862538
Nick Bohannan <nbohannan@gmail.com>
2019-02-08 18:25:16 -0600

    hometown config server

--- server.js
+++ server.js
@@ -1,3 +1,27 @@
 require(__dirname + '/Resources/config.js');
 
-console.log(config.database)
+const fs = require('fs');
+const net = require('net');
+
+//load init files
+const init_files = fs.readdirSync(__dirname + "/Initializers");
+init_files.forEach((initFile) => {
+    console.log('Loading Initializer: ' + initFile);
+    require(__dirname + "/Initializers/" + initFile);
+})
+
+//load models
+const model_files = fs.readdirSync(__dirname + "/Models");
+model_files.forEach((modelFile) => {
+    console.log('Loading Model: ' + modelFile);
+    require(__dirname + "/Models/" + modelFile);
+})
+
+//load model_file maps
+maps = {};
+const map_files = fs.readdirSync(config.data_paths.maps);
+init_files.forEach((mapFile) => {
+    console.log('Loading Map: ' + mapFile);
+    var map = require(config.data_paths + mapFile);
+    maps[map.room] = map;
+})
